bfe011a4796af92db4a17afe62d1ba026ad052a3
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-04-26 22:30:33 +0200

    Wrap propTypes for removal in production

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,46 @@
-import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
-import uglify from "rollup-plugin-uglify";
-import replace from "rollup-plugin-replace";
-import pkg from "./package.json";
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace'
+import pkg from './package.json'
 
-const minify = process.env.MINIFY;
-const format = process.env.FORMAT;
-const es = format === "es";
-const umd = format === "umd";
-const cjs = format === "cjs";
+const minify = process.env.MINIFY
+const format = process.env.FORMAT
+const es = format === 'es'
+const umd = format === 'umd'
+const cjs = format === 'cjs'
 
-let output;
+let output
 
 if (es) {
-  output = { file: `dist/react-callbag-listener.es.js`, format: "es" };
+  output = { file: `dist/react-callbag-listener.es.js`, format: 'es' }
 } else if (umd) {
   if (minify) {
     output = {
       file: `dist/react-callbag-listener.umd.min.js`,
-      format: "umd"
-    };
+      format: 'umd'
+    }
   } else {
-    output = { file: `dist/react-callbag-listener.umd.js`, format: "umd" };
+    output = { file: `dist/react-callbag-listener.umd.js`, format: 'umd' }
   }
 } else if (cjs) {
-  output = { file: `dist/react-callbag-listener.cjs.js`, format: "cjs" };
+  output = { file: `dist/react-callbag-listener.cjs.js`, format: 'cjs' }
 } else if (format) {
-  throw new Error(`invalid format specified: "${format}".`);
+  throw new Error(`invalid format specified: "${format}".`)
 } else {
-  throw new Error("no format specified. --environment FORMAT:xxx");
+  throw new Error('no format specified. --environment FORMAT:xxx')
 }
 
 export default {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: Object.assign(
     {
-      name: "react-callbag-listener",
-      exports: "named",
+      name: 'react-callbag-listener',
+      exports: 'named',
       globals: {
-        react: "React",
-        "prop-types": "PropTypes"
+        react: 'React',
+        'prop-types': 'PropTypes'
       }
     },
     output
@@ -53,20 +53,23 @@ export default {
       ],
   plugins: [
     resolve({ jsnext: true, main: true }),
-    commonjs({ include: "node_modules/**" }),
+    commonjs({ include: 'node_modules/**' }),
     babel({
-      exclude: "node_modules/**",
+      exclude: 'node_modules/**',
       babelrc: false,
-      presets: [["env", { loose: true, modules: false }], "stage-2"],
-      plugins: ["external-helpers"]
+      presets: [['env', { loose: true, modules: false }], 'stage-2'],
+      plugins: [
+        'external-helpers',
+        ['transform-react-remove-prop-types', { mode: 'unsafe-wrap' }]
+      ]
     }),
     umd
       ? replace({
-          "process.env.NODE_ENV": JSON.stringify(
-            minify ? "production" : "development"
+          'process.env.NODE_ENV': JSON.stringify(
+            minify ? 'production' : 'development'
           )
         })
       : null,
     minify ? uglify() : null
   ].filter(Boolean)
-};
+}
